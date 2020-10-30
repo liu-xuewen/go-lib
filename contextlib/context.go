9c73bd3e5171afe9e13815b39e4c73536e118bd3
liuxuewen <liiuxuewen@gmail.com>
2020-10-30 10:42:13 +0800

    feat: add contextlib

---
+++ contextlib/context.go
@@ -0,0 +1,41 @@
+package contextlib
+
+import "context"
+
+// GetString get string or default in ctx
+func GetString(ctx context.Context, key string, defaultVal string) string {
+	v := ctx.Value(key)
+	if v != nil {
+		return v.(string)
+	}
+
+	return defaultVal
+}
+
+// GetInt get string or default in ctx
+func GetInt(ctx context.Context, key string, defaultVal int) int {
+	v := ctx.Value(key)
+	if v != nil {
+		value, ok := v.(int)
+		if ok {
+			return value
+		}
+		return defaultVal
+	}
+
+	return defaultVal
+}
+
+// GetUint64 get uint64 or default in ctx
+func GetUint64(ctx context.Context, key string, defaultVal uint64) uint64 {
+	v := ctx.Value(key)
+	if v != nil {
+		value, ok := v.(uint64)
+		if ok {
+			return value
+		}
+		return defaultVal
+	}
+
+	return defaultVal
+}
--- iplib/local_ip.go
+++ iplib/local_ip.go
@@ -1,4 +1,4 @@
-package ip
+package iplib
 
 import (
 	"net"
