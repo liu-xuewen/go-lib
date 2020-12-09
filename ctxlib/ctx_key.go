a54cf92546680fd81ecd44c45a17d7de1a901970
root <root@USERUQI-P98N7M6.localdomain>
2020-12-09 21:13:08 +0800

    feat: ctx_key

---
+++ ctxlib/ctx_key.go
@@ -0,0 +1,13 @@
+package ctxlib
+
+var (
+	CtxUser             String = "ctx_user"
+	CtxKeyRequestID     String = "ctx_request_id"
+	CtxKeyRequestMethod String = "ctx_request_method"
+	CtxKeyRequestURI    String = "ctx_request_uri"
+	CtxKeyRequestPath   String = "ctx_request_path"
+	CtxKeyUserAgent     String = "ctx_user_agent"
+	CtxKeyClientIP      String = "ctx_client_ip"
+	CtxKeyResCode       String = "ctx_res_code"
+	CtxKeyResMsg        String = "ctx_res_msg"
+)
