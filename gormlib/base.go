1fbce9a3e7502581a10a4646e0e5b8767fcbe431
liuxuewen <liiuxuewen@gmail.com>
2020-12-11 12:40:36 +0800

    fix: jsontime

--- gormlib/base.go
+++ gormlib/base.go
@@ -2,12 +2,11 @@ package gormlib
 
 import (
 	"github.com/liu-xuewen/go-lib/timelib"
-	"time"
 )
 
 // Base Base
 type Base struct {
-	CreatedAt timelib.JsonTime  `gorm:"type:timestamp;index" json:"created_at"`
-	UpdatedAt timelib.JsonTime  `gorm:"type:timestamp" json:"updated_at"`
-	DeletedAt *time.Time `sql:"index"`
+	CreatedAt timelib.JsonTime  `gorm:"type:timestamp NULL;default:NULL;index" json:"created_at"`
+	UpdatedAt timelib.JsonTime  `gorm:"type:timestamp NULL;default:NULL" json:"updated_at"`
+	DeletedAt *timelib.JsonTime `gorm:"type:timestamp NULL;default:NULL;index"`
 }
